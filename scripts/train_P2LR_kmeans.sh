1e4a73ca2a54384be817e2c705dcf638b8cddcf6
hanj <jianhan.thu@gmail.com>
2022-04-08 14:00:55 +0800

    epoch=70

--- scripts/train_P2LR_kmeans.sh
+++ scripts/train_P2LR_kmeans.sh
@@ -11,13 +11,9 @@ if [ $# -ne 5 ]
     exit 1
 fi
 
-# Hint: the default epoch used in our paper in 100 for four tasks. But we currently find that epoch=60 for D2M achieves
-# similar performance. It saves training time by setting epoch to a lower value. We will test with different epochs as a
-# TODO item and update the results in the README.
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 \
 python examples/train_P2LR_kmeans.py -dt ${TARGET} -a ${ARCH} -j 16 --num-clusters ${CLUSTER} \
-	--num-instances 4 --lr 0.00035 --iters 400 -b 64 --epochs 100 --p ${P} \
+	--num-instances 4 --lr 0.00035 --iters 400 -b 64 --epochs 70 --p ${P} \
 	--soft-ce-weight 0.5 --soft-tri-weight 0.8 --dropout 0.5 --multiple_kmeans \
 	--init-1 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-1/model_best.pth.tar \
 	--init-2 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-2/model_best.pth.tar \
