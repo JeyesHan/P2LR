03e9fd963eb650e0fa444cbdb23c400ff3f1a1d4
hanj <jianhan.thu@gmail.com>
2022-04-08 14:03:04 +0800

    epoch=70

--- scripts/train_P2LR_kmeans.sh
+++ scripts/train_P2LR_kmeans.sh
@@ -11,7 +11,7 @@ if [ $# -ne 5 ]
     exit 1
 fi
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python examples/train_P2LR_kmeans.py -dt ${TARGET} -a ${ARCH} -j 16 --num-clusters ${CLUSTER} \
 	--num-instances 4 --lr 0.00035 --iters 400 -b 64 --epochs 70 --p ${P} \
 	--soft-ce-weight 0.5 --soft-tri-weight 0.8 --dropout 0.5 --multiple_kmeans \
